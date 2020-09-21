e525940e976bf404616307e06246470343c464fd
mahidulmoon <mahidulmoon@gmail.com>
2020-09-21 15:10:43 +0600

    create+view profile template

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import register,login
+from .views import register,login,createProfile,viewProfile
 urlpatterns = [
     path('login/',login,name='user-login'),
     path('register/',register,name='user-register'),
+    path('creatprofile/',createProfile,name='create-profile'),
+    path('viewprofile/',viewProfile,name='view-profile'),
 ]
--- user/views.py
+++ user/views.py
@@ -8,3 +8,12 @@ def register(request):
 
 def login(request):
     return render(request,'login.html',{})
+
+
+def createProfile(request):
+    return render(request,'create-profile.html',{})
+
+
+
+def viewProfile(request):
+    return render(request,'view-profile.html',{})
