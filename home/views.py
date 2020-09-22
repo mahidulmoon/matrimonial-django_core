5d650902f53f900e0a54969419cc7882e9555374
mahidulmoon <mahidulmoon@gmail.com>
2020-09-22 11:03:49 +0600

    add pagination in home.html( ok)

--- home/views.py
+++ home/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render
 from .models import NewEvent,SuccessStory
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 
 def home(request):
     eventlist = NewEvent.objects.all()
     storylist = SuccessStory.objects.all()
     
-    events = Paginator(eventlist,3)
-    stories = Paginator(storylist,2)
+    events = Paginator(eventlist,2)
+    stories = Paginator(storylist,3)
     page_number1 = request.GET.get('page1')
-    event_obj = events.get_page(page_number1)
+    try:
+        event_obj = events.page(page_number1)
+    except PageNotAnInteger:
+        event_obj = events.page(1)
+    except EmptyPage:
+        event_obj = events.page(events.num_pages)
+
+
     page_number2 = request.GET.get('page2')
-    story_obj = stories.get_page(page_number2)
+    try:
+        story_obj = stories.page(page_number2)
+    except PageNotAnInteger:
+        story_obj = stories.page(1)
+    except EmptyPage:
+        story_obj = stories.page(stories.num_pages)
+    
 
     context = {
         "events": event_obj,
-        "stories": story_obj,
-        "eventpage": range(events.num_pages+1)
+        "stories": story_obj
+        
     }
 
     return render(request,'index.html',context)
