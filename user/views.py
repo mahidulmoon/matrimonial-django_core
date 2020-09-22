ca88b8fc636fd2f9fbb8b4fc05e0260ddee5f070
mahidulmoon <mahidulmoon@gmail.com>
2020-09-22 12:30:23 +0600

    registration backend success

--- user/views.py
+++ user/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
 
-# Create your views here.
 
 def register(request):
-    return render(request,'register.html',{})
+    if request.method == "POST":
+        username=request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        firstname = request.POST['fname']
+        lastname = request.POST['lname'] 
+
+        if User.objects.filter(username=username).exists():
+                messages.info(request,"Username Token!!!")
+                return render(request,'register.html')
+        elif User.objects.filter(email=email).exists():
+                messages.info(request,"Email Token!!!")
+                return render(request,'register.html')
+        else:
+                user=User.objects.create_user(username=username,password=password,email=email,first_name=firstname,last_name=lastname)
+                user.save()  
+                messages.warning(request, "Registration Completed!!!")
+                return render(request,'register.html') 
+    else:
+        return render(request,'register.html')
+
+    
 
 
 def login(request):
